c994a7e6ea3cd892048f5a217c45636162118ff0
remiscarlet <remilia@scarl.et>
2022-10-11 23:27:14 +0000

    Switch to gunicorn. Websockets are temporarily broken

--- src/api/main.py
+++ src/api/main.py
@@ -3,8 +3,9 @@ from src.api import create_app
 
 from src.api.lib.sockets import socketio
 
+def main():
+    app = create_app()
+    socketio.run(app, debug=True, host="0.0.0.0", port=5001)
 
 if __name__ == "__main__":
-    app = create_app()
-
-    socketio.run(app, debug=True, host="0.0.0.0", port=5001)
+    main()
