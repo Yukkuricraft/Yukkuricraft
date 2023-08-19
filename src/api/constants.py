6fb106c1f95e63dec56da2d6aae99fa1391d7938
remiscarlet <remilia@scarl.et>
2023-08-19 18:22:07 -0400

    yakumo2/api2 -> yakumo/api

--- src/api/constants.py
+++ src/api/constants.py
@@ -4,7 +4,7 @@ G_CLIENT_ID = (
     "1084736521175-2b5rrrpcs422qdc5458dhisdsj8auo0p.apps.googleusercontent.com"
 )
 
-CORS_ORIGIN = "https://yakumo2.yukkuricraft.net"
+CORS_ORIGIN = "https://yakumo.yukkuricraft.net"
 
 DB_ENV_FILE = "secrets/db.env"
 
