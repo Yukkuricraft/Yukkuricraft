470882c63c392468c04af6204ca0ce3df01bbec7
remiscarlet <remilia@scarl.et>
2024-03-20 00:13:55 -0400

    Add bukkit.yml template

--- src/generator/constants.py
+++ src/generator/constants.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+BUKKIT_YML_TEMPLATE_PATH: Path = Path("templates/bukkit.tpl.yml")
 PAPER_GLOBAL_TEMPLATE_PATH: Path = Path("templates/paper-global.tpl.yml")
 DOCKER_COMPOSE_TEMPLATE_PATH: Path = Path("templates/docker-compose.tpl.yml")
 VELOCITY_CONFIG_TEMPLATE_PATH: Path = Path("templates/velocity.tpl.toml")
