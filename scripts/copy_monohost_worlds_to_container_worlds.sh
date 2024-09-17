ae3ae5b117ca1186083d873954a30e3557bbf0cd
remiscarlet <remilia@scarl.et>
2024-09-17 11:42:05 -0700

    Update YC 1.0->2.0 world copy script

--- scripts/copy_monohost_worlds_to_container_worlds.sh
+++ scripts/copy_monohost_worlds_to_container_worlds.sh
@@ -1,32 +1,61 @@
 #!/usr/bin/env bash
 
-MONOYC_BASE=/home/minecraft/YC/YukkuriCraft
+ENV_NAME="env2"
 
-CONTAINER_YC_BASE=/var/lib/yukkuricraft/env/prod
+MONOYC_BASE="/home/minecraft/YC/YukkuriCraft"
 
-# Survival
-rsync -arP "$MONOYC_BASE/Survival4" "$CONTAINER_YC_BASE/survival/worlds/"
-rsync -arP "$MONOYC_BASE/Survival4_nether" "$CONTAINER_YC_BASE/survival/worlds/"
-rsync -arP "$MONOYC_BASE/Survival4_the_end" "$CONTAINER_YC_BASE/survival/worlds/"
+CONTAINER_YC_BASE="/var/lib/yukkuricraft/env/$ENV_NAME/minecraft"
+
+SURVIVAL_WORLDGROUP="yukkuricraft"
+mkdir -p "$CONTAINER_YC_BASE/$SURVIVAL_WORLDGROUP"
+rsync -arP "$MONOYC_BASE/Survival4" "$CONTAINER_YC_BASE/$SURVIVAL_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/Survival4_nether" "$CONTAINER_YC_BASE/$SURVIVAL_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/Survival4_the_end" "$CONTAINER_YC_BASE/$SURVIVAL_WORLDGROUP/worlds/"
 
 # Creative
-rsync -arP "$MONOYC_BASE/CreativeFlat" "$CONTAINER_YC_BASE/creative/worlds/"
-rsync -arP "$MONOYC_BASE/Statues" "$CONTAINER_YC_BASE/creative/worlds/"
-rsync -arP "$MONOYC_BASE/buildingContest" "$CONTAINER_YC_BASE/creative/worlds/"
+CREATIVE_WORLDGROUP="yukkuricraft"
+mkdir -p "$CONTAINER_YC_BASE/$CREATIVE_WORLDGROUP"
+rsync -arP "$MONOYC_BASE/CreativeFlat" "$CONTAINER_YC_BASE/$CREATIVE_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/Statues" "$CONTAINER_YC_BASE/$CREATIVE_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/buildingContest" "$CONTAINER_YC_BASE/$CREATIVE_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/Dungeons" "$CONTAINER_YC_BASE/$MINIGAMES_AND_EVENTS_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/SummerHeight" "$CONTAINER_YC_BASE/$MINIGAMES_AND_EVENTS_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/NyadminFlats" "$CONTAINER_YC_BASE/$MINIGAMES_AND_EVENTS_WORLDGROUP/worlds/"
 
 # Old Gensokyo
-rsync -arP "$MONOYC_BASE/Gensokyo" "$CONTAINER_YC_BASE/old-gensokyo/worlds/"
-rsync -arP "$MONOYC_BASE/NewHaku" "$CONTAINER_YC_BASE/old-gensokyo/worlds/"
-rsync -arP "$MONOYC_BASE/Heaven" "$CONTAINER_YC_BASE/old-gensokyo/worlds/"
-rsync -arP "$MONOYC_BASE/Makai" "$CONTAINER_YC_BASE/old-gensokyo/worlds/"
-rsync -arP "$MONOYC_BASE/Senkai" "$CONTAINER_YC_BASE/old-gensokyo/worlds/"
+OLDGENSOKYO_WORLDGROUP="yukkuricraft"
+mkdir -p "$CONTAINER_YC_BASE/$OLDGENSOKYO_WORLDGROUP"
+rsync -arP "$MONOYC_BASE/Gensokyo" "$CONTAINER_YC_BASE/$OLDGENSOKYO_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/NewHaku" "$CONTAINER_YC_BASE/$OLDGENSOKYO_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/Heaven" "$CONTAINER_YC_BASE/$OLDGENSOKYO_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/Makai" "$CONTAINER_YC_BASE/$OLDGENSOKYO_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/Senkai" "$CONTAINER_YC_BASE/$OLDGENSOKYO_WORLDGROUP/worlds/"
 
 # Neo Gensokyo
-rsync -arP "$MONOYC_BASE/NeoGensokyo" "$CONTAINER_YC_BASE/neo-gensokyo/worlds/"
+NEOGENSOKYO_WORLDGROUP="yukkuricraft"
+mkdir -p "$CONTAINER_YC_BASE/$NEOGENSOKYO_WORLDGROUP"
+rsync -arP "$MONOYC_BASE/NeoGensokyo" "$CONTAINER_YC_BASE/$NEOGENSOKYO_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/Genso35" "$CONTAINER_YC_BASE/$NEOGENSOKYO_WORLDGROUP/worlds/"
 
 # Minigames and Events
-rsync -arP "$MONOYC_BASE/PB_Gensokyo" "$CONTAINER_YC_BASE/minigames-and-events/worlds/"
+MINIGAMES_AND_EVENTS_WORLDGROUP="yukkuricraft"
+mkdir -p "$CONTAINER_YC_BASE/$MINIGAMES_AND_EVENTS_WORLDGROUP"
+rsync -arP "$MONOYC_BASE/PB_Gensokyo" "$CONTAINER_YC_BASE/$MINIGAMES_AND_EVENTS_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/EventWorld" "$CONTAINER_YC_BASE/$MINIGAMES_AND_EVENTS_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/halloween_2023" "$CONTAINER_YC_BASE/$MINIGAMES_AND_EVENTS_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/YukkuriKart" "$CONTAINER_YC_BASE/$MINIGAMES_AND_EVENTS_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/Halloween2021" "$CONTAINER_YC_BASE/$MINIGAMES_AND_EVENTS_WORLDGROUP/worlds/"
+
+# Retro Gensokyo Worlds
+RETRO_SURVIVAL_WORLDS="yukkuricraft"
+mkdir -p "$CONTAINER_YC_BASE/$RETRO_SURVIVAL_WORLDS"
+rsync -arP "$MONOYC_BASE/Survival-12.29.13" "$CONTAINER_YC_BASE/$RETRO_SURVIVAL_WORLDS/worlds/"
+rsync -arP "$MONOYC_BASE/NeoSurvival-7.24.2016" "$CONTAINER_YC_BASE/$RETRO_SURVIVAL_WORLDS/worlds/"
+rsync -arP "$MONOYC_BASE/NovaSurvival-7.7.2019" "$CONTAINER_YC_BASE/$RETRO_SURVIVAL_WORLDS/worlds/"
 
 # Lobby
-rsync -arP "$MONOYC_BASE/GapRealm" "$CONTAINER_YC_BASE/lobby/worlds/"
-rsync -arP "$MONOYC_BASE/BackDoorRealm" "$CONTAINER_YC_BASE/lobby/worlds/"
+LOBBY_WORLDGROUP="yukkuricraft"
+mkdir -p "$CONTAINER_YC_BASE/$LOBBY_WORLDGROUP"
+rsync -arP "$MONOYC_BASE/GapRealm" "$CONTAINER_YC_BASE/$LOBBY_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/BackDoorRealm" "$CONTAINER_YC_BASE/$LOBBY_WORLDGROUP/worlds/"
+rsync -arP "$MONOYC_BASE/Dreamworld" "$CONTAINER_YC_BASE/$LOBBY_WORLDGROUP/worlds/"
