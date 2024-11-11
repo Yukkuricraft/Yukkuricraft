c65d9999cce94f65396e6913da98406a298c0a4d
remiscarlet <remilia@scarl.et>
2024-11-10 20:19:57 -0800

    Update chown_restic_dir_then_entrypoint to use UID/GID rather than names

--- scripts/chown_restic_dir_then_entrypoint.sh
+++ scripts/chown_restic_dir_then_entrypoint.sh
@@ -2,13 +2,19 @@
 
 set -e
 
-user=${HOSTUSER:-hostuser}
+host_uid=${HOST_UID:-NO_HOST_UID}
+host_gid=${HOST_GID:-NO_HOST_GID}
+username=$(cat /etc/passwd | grep :${host_uid}: | cut -d\: -f1)
+
+echo "host_uid: $host_uid"
+echo "host_gid: $host_gid"
+echo "username: $username"
 
 mount_path=${RESTIC_REPOSITORY}
 mount_path_owner=$(ls -ld ${mount_path} | awk '{print $3}')
-if [ "${user}" != "${mount_path_owner}" ]; then
-  chown -R ${user}:${user} ${mount_path}
+if [ "${username}" != "${mount_path_owner}" ]; then
+  chown -R ${uid}:${gid} ${mount_path}
 fi
 
-echo exec su-exec ${user} ${ENTRYPOINT_TARGET}
-exec su-exec ${user} ${ENTRYPOINT_TARGET}
+echo exec su-exec ${username} ${ENTRYPOINT_TARGET}
+exec su-exec ${username} ${ENTRYPOINT_TARGET}
